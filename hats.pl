2df1cb513628d617f8811a0a2606df9cd57818b3
protospork <protospork@gmail.com>
2015-04-05 10:24:38 -0400

    initial commit
    why am I doing this

---
+++ hats.pl
@@ -0,0 +1,94 @@
+# player can spend 10 banked hats to give a fedora to someone (fedoras are bad)
+# fedoras are permanent? random fedora theft incidents? bronies?
+
+
+#TODO: (in order of importance)
+#fedoras
+#port database to SQL
+#find a nick that doesn't blow
+#rate limiting
+#uhh does perl have a problem with huge numbers?
+
+use vars qw($VERSION %IRSSI);
+use Modern::Perl;
+use Tie::YAML;
+
+$VERSION = "0.1.2";
+%IRSSI = (
+    authors => 'protospork',
+    contact => 'https://github.com/protospork',
+    name => 'hats',
+    description => 'a dumb game(?)',
+    license => 'MIT/X11'
+);
+Irssi::settings_add_str('hatbot', 'hat_channels', '#wat');
+
+
+
+tie my %hats, 'Tie::YAML', $ENV{HOME}.'/.irssi/scripts/cfg/hats.po' or die $!;
+
+sub event_privmsg {
+	my ($server, $data, $nick, $mask) = @_;
+	my ($target, $text) = split(/ :/, $data, 2);
+
+	my @enabled_chans = split /,/, Irssi::settings_get_str('hat_channels');
+	return unless grep lc $target eq lc $_, (@enabled_chans);
+
+	if ($text =~ /^\s*\.hats?$/i){
+		#continue
+	} elsif ($text =~ /^\s*\.fedora$/i){
+		return; #for now
+	} else {
+		return;
+	}
+
+	my $return = (give_hats($nick))[0];
+	$server->command("action $target $return");
+}
+sub give_hats {
+	my $hats = 1;
+	$hats += int(rand(5));
+
+	my $past_hats = 0;
+	if (exists $hats{$_[0]}{'hats'}){
+		$past_hats = $hats{$_[0]}{'hats'};
+	} else {
+		$hats{$_[0]}{'hats'} = 0;
+	}
+
+
+	if (exists $hats{$_[0]}{'last_time'}){
+		if (time - $hats{$_[0]}{'last_time'} < 86400){
+			my $no = 'thinks '.$_[0].' should be content with '.$hats{$_[0]}{'hats'}.' hat';
+			$no = pluralize($no);
+			
+			return $no;
+		}
+	} else {
+		$hats{$_[0]}{'last_time'} = 0;
+	}
+
+	$hats{$_[0]}{'last_time'} = time;
+	
+	my $new_hats = $hats + $past_hats;
+	$hats{$_[0]}{'hats'} = $new_hats;
+
+	tied(%hats)->save;
+
+	my $out = 'gives '.$hats.' hats to '.$_[0].' for a total of '.$new_hats.' hat';
+	$out = pluralize($out);
+	return ($out, $hats, $new_hats);
+}
+sub pluralize {
+	my $string = $_[0];
+	my $num = $string;
+	$num =~ s/^.+(\d+).*?$/$1/;
+
+	$num > 1 # why does this work...
+	? $string .= 's'
+	: $string .= '.';
+
+	return $string;
+}
+
+Irssi::signal_add("event privmsg", "event_privmsg");
